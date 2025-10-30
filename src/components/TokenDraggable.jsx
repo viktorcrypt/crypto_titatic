514c2aaaa33dca258c54cfdcea089513fea97319
GrishaG <118081038+viktorcrypt@users.noreply.github.com>
2025-10-30 12:44:16 +0100

    Update TokenDraggable.jsx

--- src/components/TokenDraggable.jsx
+++ src/components/TokenDraggable.jsx
@@ -1,11 +1,10 @@
-
 export default function TokenDraggable({ token, pos }) {
   function handleDragStart(e) {
     e.dataTransfer.setData("text/plain", token.symbol);
-    
+
     const img = new Image();
-    img.src = `/logos/${token.symbol.toLowerCase()}.svg`;
-    e.dataTransfer.setDragImage(img, 16, 16);
+    img.src = /logos/${token.symbol.toLowerCase()}.svg;
+    e.dataTransfer.setDragImage(img, 24, 24); 
   }
 
   return (
@@ -14,14 +13,19 @@ export default function TokenDraggable({ token, pos }) {
       onDragStart={handleDragStart}
       className="absolute cursor-grab active:cursor-grabbing"
       style={{ left: pos.left, top: pos.top }}
-      title={`${token.name} (${token.symbol})`}
+      title={${token.name} (${token.symbol})}
     >
-      
       <div style={{ transform: "rotate(10deg)" }}>
         <img
-          src={`/logos/${token.symbol.toLowerCase()}.svg`}
-          className="h-10 w-10 rounded-full ring-2 ring-white/50 bg-white shadow"
+          src={/logos/${token.symbol.toLowerCase()}.svg}
+          className="rounded-full ring-2 ring-white/50 bg-white shadow"
           draggable={false}
+          style={{
+            width: "40px",
+            height: "40px",
+            objectFit: "contain",
+            objectPosition: "center",
+          }}
         />
       </div>
     </div>
